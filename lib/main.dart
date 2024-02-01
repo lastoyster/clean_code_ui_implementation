78cf7cee2ea803b5155ca7191d8e7b05d08594fe
lastoyster <kadampratiksha56@gmail.com>
2024-02-01 13:10:48 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:clean_ui_code_implementation/src/product_detail/product_detail_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Clean UI Code Implementation',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const ProductDetailPage(),
+      // home: const ProductDetailPagePrev(),
+    );
+  }
+}
